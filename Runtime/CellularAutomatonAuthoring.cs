71d2af8c20beb5fccfedd6278df278850e23cc32
Christopher Atkinson <c_atkinson@outlook.com>
2022-04-10 18:48:08 +0100

    created basic scene setup to fun default shader and display image

--- Runtime/CellularAutomatonAuthoring.cs
+++ Runtime/CellularAutomatonAuthoring.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ChristopherAtkinson.CellularAutomaton
 {
     public class CellularAutomatonAuthoring : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private RenderTexture m_RenderTexture;
+
+        [SerializeField] private ComputeShader m_ComputeShader;
+
+        private void OnEnable()
         {
-        
+            RenderTexture renderTexture = new RenderTexture(m_RenderTexture);
+            renderTexture.enableRandomWrite = true;
+
+            var kernel = m_ComputeShader.FindKernel("CSMain");
+            m_ComputeShader.SetTexture(kernel, "Result", renderTexture);
+            m_ComputeShader.Dispatch(kernel, (renderTexture.width / 32) + 1, (renderTexture.height / 32) + 1, 1);
+
+            if (TryGetComponent(out RawImage rawImage))
+                rawImage.texture = renderTexture;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDisable()
         {
-        
+            m_RenderTexture.Release();
         }
     }
 }
